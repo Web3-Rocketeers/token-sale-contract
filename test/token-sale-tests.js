2d7adc776ae26f6d91b2fa324f39defb537768b0
Fonck <AFonck@protonmail.com>
2023-04-16 20:35:04 -0600

    tests-config.

--- test/token-sale-tests.js
+++ test/token-sale-tests.js
@@ -1,29 +1,45 @@
 const { expect } = require("chai");
 const { BN, ether, expectEvent, expectRevert, constants } = require("@openzeppelin/test-helpers");
 const TokenSale = artifacts.require("TokenSale");
-const ERC20 = artifacts.require("IERC20");
+const truffleConfig = require('../truffle-config');
 
 contract("TokenSale", function (accounts) {
-    const [deployer, buyer, other] = accounts;
-    let tokenSale, token;
-  
-    beforeEach(async function () {
-      // Deploy the token contract and give some tokens to the token sale contract
-      token = await ERC20.new("Test Token", "TST", 18, { from: deployer });
-      const initialSupply = ether("1000");
-      await token.transfer(tokenSale.address, initialSupply, { from: deployer });
-  
-      // Deploy the token sale contract with the token address and a token price
-      const tokenPrice = ether("0.000005");
-      tokenSale = await TokenSale.new(token.address, tokenPrice, { from: deployer });
-    });
-  
-    it("should initialize the token sale contract with correct values", async function () {
-      expect(await tokenSale.admin()).to.equal(deployer);
-      expect(await tokenSale.tokenContract()).to.equal(token.address);
-      expect(await tokenSale.tokenPrice()).to.be.bignumber.equal(tokenPrice);
-    });
-  
-    // ...add more test cases
+  const [deployer, buyer, other] = accounts;
+  let tokenSale, token;
+
+  beforeEach(async function () {
+    const network = await web3.eth.net.getNetworkType();
+    const tokenABI = truffleConfig.getABI(network);
+    const deployedTokenAddress = /* Your deployed token address */;
+
+    // Create a contract instance using the ABI and the token contract address
+    token = new web3.eth.Contract(tokenABI, deployedTokenAddress);
+
+    // Deploy TokenSale contract
+    tokenSale = await TokenSale.new(deployedTokenAddress);
+
+    // Transfer some tokens to the TokenSale contract
+    const tokensToTransfer = web3.utils.toBN(1e18);
+    await token.methods.transfer(tokenSale.address, tokensToTransfer.toString()).send({ from: deployer });
   });
-  
+
+  it("should correctly initialize the token sale contract", async function () {
+    const tokenAddress = await tokenSale.tokenContract();
+    assert.equal(tokenAddress, token.options.address, "TokenSale contract has incorrect token address");
+  });
+
+  it("should allow users to buy tokens", async function () {
+    const tokensToBuy = web3.utils.toBN(1e17);
+    const tokenSaleRate = await tokenSale.rate();
+    const value = tokensToBuy.mul(tokenSaleRate);
+    
+    // Buy tokens
+    await tokenSale.buyTokens(tokensToBuy.toString(), { from: buyer, value: value.toString() });
+
+    // Check buyer balance
+    const buyerBalance = await token.methods.balanceOf(buyer).call();
+    assert.equal(buyerBalance.toString(), tokensToBuy.toString(), "Buyer has incorrect token balance after purchase");
+  });
+
+  // Add more test cases as needed
+});

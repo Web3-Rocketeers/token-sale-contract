755398ccfc30b4fd0bc22533b2964c9173ced61d
Fonck <AFonck@protonmail.com>
2023-06-14 15:18:19 -0600

    testing improvements.

--- test/token-sale-tests.js
+++ test/token-sale-tests.js
@@ -1,62 +1,41 @@
-// token-sale-tests.js
-
 const { expect } = require("chai");
 const { BN, ether, expectEvent, expectRevert, constants } = require("@openzeppelin/test-helpers");
 const TokenSale = artifacts.require("TokenSale");
-const truffleConfig = require('../truffle-config');
+const Token = artifacts.require("Token"); // Replace "Token" with the name of your token contract
 
-contract("TokenSale", function (accounts) {
-  const [deployer, buyer, other] = accounts;
-  let tokenSale, token;
+contract("TokenSale", function ([deployer, buyer, other]) {
+  let tokenSale;
+  let token;
 
   beforeEach(async function () {
-    const network = await web3.eth.net.getNetworkType();
-    const tokenABI = truffleConfig.getABI(network);
-    const deployedTokenAddress = 0xcEAAc35a8992CA451a85A377C831eD9Bc45026e1;
+    // Deploy the token contract
+    token = await Token.new(/* parameters */);
 
-    // Create a contract instance using the ABI and the token contract address
-    token = new web3.eth.Contract(tokenABI, deployedTokenAddress);
-    console.log("Token contract instance created");
+    // Mint some tokens to the deployer
+    await token.mint(deployer, ether("1000"));
 
-    // Deploy TokenSale contract
-    tokenSale = await TokenSale.new(deployedTokenAddress);
-    console.log("TokenSale contract deployed");
+    // Deploy the TokenSale contract
+    tokenSale = await TokenSale.new(token.address, ether("1"), { from: deployer });
 
     // Transfer some tokens to the TokenSale contract
-    const tokensToTransfer = web3.utils.toBN(1e18);
-    await token.methods.transfer(tokenSale.address, tokensToTransfer.toString()).send({ from: deployer });
-    console.log("Tokens transferred to TokenSale contract");
+    await token.transfer(tokenSale.address, ether("500"), { from: deployer });
   });
 
   it("should correctly initialize the token sale contract", async function () {
-    const tokenAddress = await tokenSale.tokenContract();
-    assert.equal(tokenAddress, token.options.address, "TokenSale contract has incorrect token address");
-    console.log("TokenSale contract initialization checked");
+    expect(await tokenSale.token()).to.equal(token.address);
+    expect(await tokenSale.price()).to.be.bignumber.equal(ether("1"));
   });
 
   it("should allow users to buy tokens", async function () {
-    const tokensToBuy = web3.utils.toBN(1e17);
-    const tokenSaleRate = await tokenSale.rate();
-    const value = tokensToBuy.mul(tokenSaleRate);
-    
-    // Buy tokens
-    await tokenSale.buyTokens(tokensToBuy.toString(), { from: buyer, value: value.toString() });
-    console.log("Tokens bought");
-
-    // Check buyer balance
-    const buyerBalance = await token.methods.balanceOf(buyer).call();
-    assert.equal(buyerBalance.toString(), tokensToBuy.toString(), "Buyer has incorrect token balance after purchase");
-    console.log("Buyer balance checked");
+    let amountToBuy = ether("1");
+    await tokenSale.buyTokens({ from: buyer, value: amountToBuy });
+    expect(await token.balanceOf(buyer)).to.be.bignumber.equal(amountToBuy);
   });
 
   it("should not allow users to buy more tokens than available", async function () {
-    const tokensToBuy = web3.utils.toBN(2e18); // More than the tokens transferred to the contract
-    const tokenSaleRate = await tokenSale.rate();
-    const value = tokensToBuy.mul(tokenSaleRate);
-
-    // Attempt to buy tokens
+    let amountToBuy = ether("1000"); // More than the tokens transferred to the contract
     await expectRevert(
-      tokenSale.buyTokens(tokensToBuy.toString(), { from: buyer, value: value.toString() }),
+      tokenSale.buyTokens({ from: buyer, value: amountToBuy }),
       "Not enough tokens left for sale"
     );
   });
@@ -66,38 +45,31 @@ contract("TokenSale", function (accounts) {
       tokenSale.endSale({ from: other }),
       "Ownable: caller is not the owner"
     );
-  });
 
-  it("should transfer remaining tokens and Ether to owner on endSale", async function () {
-    const initialOwnerBalance = await token.methods.balanceOf(deployer).call();
-    const initialContractBalance = await token.methods.balanceOf(tokenSale.address).call();
+    const initialOwnerBalance = await token.balanceOf(deployer);
+    const initialContractBalance = await token.balanceOf(tokenSale.address);
 
-    // End sale
     await tokenSale.endSale({ from: deployer });
 
-    const finalOwnerBalance = await token.methods.balanceOf(deployer).call();
-    const finalContractBalance = await token.methods.balanceOf(tokenSale.address).call();
+    const finalOwnerBalance = await token.balanceOf(deployer);
+    const finalContractBalance = await token.balanceOf(tokenSale.address);
 
-    assert.equal(finalContractBalance, 0, "Contract should have no tokens left");
-    assert.equal(finalOwnerBalance - initialOwnerBalance, initialContractBalance, "Owner should have received the remaining tokens");
+    expect(finalContractBalance).to.be.bignumber.equal("0");
+    expect(finalOwnerBalance.sub(initialOwnerBalance)).to.be.bignumber.equal(initialContractBalance);
   });
 
   it("should allow only owner to set the price", async function () {
-    const newPrice = web3.utils.toWei("0.00001", "ether");
-  
-    // Attempt to set price from non-owner account
+    const newPrice = ether("0.01");
+
     await expectRevert(
       tokenSale.setPrice(newPrice, { from: other }),
       "Ownable: caller is not the owner"
     );
-  
-    // Set price from owner account
+
     await tokenSale.setPrice(newPrice, { from: deployer });
-  
-    // Check that the price has been updated
-    const tokenSalePrice = await tokenSale.price();
-    assert.equal(tokenSalePrice.toString(), newPrice, "Price was not updated correctly");
+
+    expect(await tokenSale.price()).to.be.bignumber.equal(newPrice);
   });
-  
-  // Add more test cases as needed
+
+  // Add more tests as needed
 });
